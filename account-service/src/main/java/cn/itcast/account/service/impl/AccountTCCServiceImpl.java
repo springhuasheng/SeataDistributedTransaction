8f9f7b104d2ee06ed9084e225f5dc55c1c83d685
liyunfei <lyflang@163.com>
2022-01-16 18:50:04 +0800

    更新集群配置

---
+++ account-service/src/main/java/cn/itcast/account/service/impl/AccountTCCServiceImpl.java
@@ -0,0 +1,88 @@
+package cn.itcast.account.service.impl;
+
+import cn.itcast.account.entity.AccountFreeze;
+import cn.itcast.account.mapper.AccountFreezeMapper;
+import cn.itcast.account.mapper.AccountMapper;
+import cn.itcast.account.service.AccountTCCService;
+import io.seata.core.context.RootContext;
+import io.seata.rm.tcc.api.BusinessActionContext;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Slf4j
+public class AccountTCCServiceImpl implements AccountTCCService {
+
+    @Autowired
+    private AccountMapper accountMapper;
+    @Autowired
+    private AccountFreezeMapper freezeMapper;
+
+    @Override
+    @Transactional
+    public void deduct(String userId, int money) {
+        // 0.获取事务id
+        String xid = RootContext.getXID();
+
+        // 业务悬挂
+        // 如果之前没有执行过try 但是已经执行过cancel 这里就要拒绝业务
+        AccountFreeze oldFreeze = freezeMapper.selectById(xid);
+        if (oldFreeze != null) {
+            return;
+        }
+
+        // 1.扣减可用余额
+        accountMapper.deduct(userId, money);
+        // 2.记录冻结金额，事务状态
+        AccountFreeze freeze = new AccountFreeze();
+        freeze.setUserId(userId);
+        freeze.setFreezeMoney(money);
+        freeze.setState(AccountFreeze.State.TRY);
+        freeze.setXid(xid);
+        freezeMapper.insert(freeze);
+    }
+
+    @Override
+    public boolean confirm(BusinessActionContext ctx) {
+        // 1.获取事务id
+        String xid = ctx.getXid();
+        // 2.根据id删除冻结记录
+        int count = freezeMapper.deleteById(xid);
+        return count == 1;
+    }
+
+    @Override
+    public boolean cancel(BusinessActionContext ctx) {
+        // 0.查询冻结记录
+        String xid = ctx.getXid();
+        AccountFreeze freeze = freezeMapper.selectById(xid);
+
+        // 空回滚
+        // 判断freeze是否为null 如果是则说明try没有执行 需要进行空回滚
+        if (freeze == null) {
+            freeze = new AccountFreeze();
+            freeze.setUserId(ctx.getActionContext("userId").toString());
+            freeze.setFreezeMoney(0);
+            freeze.setState(AccountFreeze.State.CANCEL);
+            freeze.setXid(xid);
+            freezeMapper.insert(freeze);
+            return true;
+        }
+
+        // 幂等性判断
+        if (freeze.getState() == AccountFreeze.State.CANCEL) {
+            // 说明已经处理过 不要重复处理
+            return true;
+        }
+
+        // 1.恢复可用余额
+        accountMapper.refund(freeze.getUserId(), freeze.getFreezeMoney());
+        // 2.将冻结金额清零，状态改为CANCEL
+        freeze.setFreezeMoney(0);
+        freeze.setState(AccountFreeze.State.CANCEL);
+        int count = freezeMapper.updateById(freeze);
+        return count == 1;
+    }
+}
